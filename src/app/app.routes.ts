3085c14e1cf7caebc7dcf28f2d8df774ed159b90
Ferneka <fernandadcaguiar@gmail.com>
2024-05-22 12:17:43 -0300

    savingChanges

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,24 @@
 import { Routes } from '@angular/router';
 import { CategoryListComponent } from './features/category/category-list/category-list.component';
 import { AddCategoryComponent } from './features/category/add-category/add-category.component';
+import { ProductListComponent } from './features/product/product-list/product-list.component';
+import { AddProductComponent } from './features/product/add-product/add-product.component';
 
 export const routes: Routes = [
     {
-       path: "admin/categories",
-       component: CategoryListComponent
+        path: "category-list",
+        component: CategoryListComponent
     },
     {
-        path: "admin/categories/add",
+        path : "add-category",
         component : AddCategoryComponent
+    },
+    {
+        path: "product-list",
+        component : ProductListComponent
+    },
+    {
+        path : "add-product",
+        component : AddProductComponent
     }
 ];
--- src/app/features/category/category-list/category-list.component.ts
+++ src/app/features/category/category-list/category-list.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Category } from '../models/category.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -10,4 +12,22 @@ import { RouterLink } from '@angular/router';
 })
 export class CategoryListComponent {
 
+  categories?: Category[];
+
+  constructor(private categoryService : CategoryService){
+
+  }
+
+  // ngOnInit(): void {
+  //   this.categoryService.getAllCategories()
+  //   .subscribe({
+  //     next : (response) => {
+  //       this.categories = response;
+  //     }
+
+  //   })
+  // };
+
 }
+
+
